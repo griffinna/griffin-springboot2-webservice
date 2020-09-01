80ba51fa9d88e7a8ed683e36661319d6e69b1579
Griffin <garam.dev9@gmail.com>
2020-09-01 21:25:40 +0900

    글등록 API

---
+++ src/main/java/com/griffin/springboot/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.griffin.springboot.service.posts;
+
+import com.griffin.springboot.domain.posts.PostsRepository;
+import com.griffin.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/main/java/com/griffin/springboot/web/PostsApiController.java
@@ -0,0 +1,21 @@
+package com.griffin.springboot.web;
+
+import com.griffin.springboot.service.posts.PostsService;
+import com.griffin.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+}
