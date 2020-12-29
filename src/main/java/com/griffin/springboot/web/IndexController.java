abc4f2301af88fffd6d9365a860d2e16a72ae733
Griffin <garam.dev9@gmail.com>
2020-12-29 19:41:46 +0900

    Mustache 로 화면구성 (index, write)

---
+++ src/main/java/com/griffin/springboot/web/IndexController.java
@@ -0,0 +1,21 @@
+package com.griffin.springboot.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+}
