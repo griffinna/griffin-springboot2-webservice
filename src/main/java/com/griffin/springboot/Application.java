4663b63535a3e0bd9156b5f4379161b0403c6625
Griffin <garam.dev9@gmail.com>
2020-12-31 21:59:19 +0900

    테스트에 Secutiry 적용 - @WebMvcTest 에서 CustomOAuth2UserService 찾을 수 없음
    . @WebMvcTest : 스캔범위에서 SecurityConfig 제외
    . @EnableJpaAuditing : @SpringBootApplication 과 분리 (JpaConfig)

--- src/main/java/com/griffin/springboot/Application.java
+++ src/main/java/com/griffin/springboot/Application.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing      // JPA Auditing 활성화
+// @WebMvcTest 에서 @Entity 클래스가 없어 에러 발생 (@EnableJpaAuditing 과 @SpringBootApplication 을 분리) > @EnableJpaAuditing 제거 후 JpaConfig 에 작성
+//@EnableJpaAuditing      // JPA Auditing 활성화
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
--- src/test/java/com/griffin/springboot/web/HelloControllerTest.java
+++ src/test/java/com/griffin/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.griffin.springboot.web;
 
+import com.griffin.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.hamcrest.Matchers.is;
@@ -11,12 +15,17 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,    // @ContorllerAdvice, @Controller 를 Scan (@Repository, @Service, @Component 는 스캔대상이 아님)
+        // SecurityConfig 를 생성하기 위해 필요한 CustomOAuth2UserService 는 읽을 수 없음 (스캔범위에서 제거)
+        excludeFilters = {
+            @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        })
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles = "USER")
     @Test
     public void Hello가_리턴된다() throws Exception {
         String hello = "hello";
@@ -26,6 +35,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles = "USER")
     @Test
     public void HelloDto가_리턴된다() throws Exception {
         String name = "hello";
